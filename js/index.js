95ac4ec4dec2b97a8d54eb86fcc347ab0766ccda
vakninjo <vakninjo@gmail.com>
2018-05-21 16:53:18 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,14 +5,3 @@ $(document).ready(function(){
     });
 });
 
-// console.log(logo);
-// logo.onclick = window.location.replace("http://www.pentestjo.info/");
-// logo.addEventListener("click", function(){ window.location.replace("http://www.pentestjo.info/")});
-// logo.onclick="redirect()";
-//
-// function redirect() {
-//   alert("clicked");
-//
-// }
-
-// addEvent(logo, "click", function(){ alert("clicked")});
