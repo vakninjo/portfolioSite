8698defd5bf7927fc1698cca234e93b9390d9ec3
vakninjo <vakninjo@gmail.com>
2018-05-22 22:54:14 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".logoImg").on("click", function(){
       let logo = document.getElementsByClassName("logoImg");
-      document.location.href = "http://pentestjo.info/index.html";
+      document.location.href = "http://www.pentestjo.info/";
     });
 });
 
